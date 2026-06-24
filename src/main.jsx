8fa66f8625867d96c2cb5882e70400e6cf388d3a
Shubham Sharma <shubhamshwork@outlook.com>
2026-06-24 15:48:53 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './Layout.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: App,
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
