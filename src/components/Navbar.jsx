6801c4a5ab11aaed5144e7de48c628a981fdeb79
Shubham Sharma <shubhamshwork@outlook.com>
2026-06-24 16:32:57 +0530

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <>
-            <h1>Navbar</h1>
+            <nav className="flex justify-between h-[70px] items-center px-4 sm:px-8">
+                <p className="font-bold text-[18px]">TeachForm</p>
+                <ul className="hidden sm:flex gap-8 sm:items-center">
+                    <p>Pricing</p>
+                    <p>Contact</p>
+                    <p className="border-b-3 leading-7">Get Started</p>
+                </ul>
+                <div className="sm:hidden">
+                    <GiHamburgerMenu size={28} onClick={() => setShowMenu(!showMenu)}/>
+                </div>
+            </nav>
+            {
+                showMenu &&
+                <ul className="flex-col px-4">
+                    <p className="py-[15px]">Pricing</p>
+                    <p className="py-[15px]">Contact</p>
+                    <p className="py-[15px]">Get Started</p>
+                </ul>
+            }
         </>
     );
 }
