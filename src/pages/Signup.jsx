ba25c266edbfd7e9c2f35121c5e6c31fcf43c409
Shubham Sharma <shubhamshwork@outlook.com>
2026-06-24 19:17:43 +0530

    Add signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,22 @@
+import Heading from "../components/ui/Heading";
+import Input from "../components/ui/Input";
+import { NavText } from "../components/ui/NavText";
+import SubmitButton from "../components/ui/SubmitButton";
+
+export default function Signup () {
+    return (
+        <div className="flex justify-center sm:items-center sm:h-[90vh]">
+            <div className="w-[300px] sm:w-[500px]">
+                <Heading text="Sign up"/>
+                <form className="flex flex-col gap-6 items-center">
+                    <Input type="text" placeholder="Enter your name" />
+                    <Input type="tel" placeholder="Enter your phone number" />
+                    <Input type="password" placeholder="Enter new password" />
+                    <Input type="password" placeholder="Confirm password" />
+                    <SubmitButton text="Sign up"/>
+                    <NavText text="Already have an account ?" linkText="Log in" link="/auth/login"/>
+                </form>
+            </div>
+        </div>
+    )
+}
