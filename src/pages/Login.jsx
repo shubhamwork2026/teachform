798c062201d2eed60695a6d6fb52a8134c1063b2
Shubham Sharma <shubhamshwork@outlook.com>
2026-06-24 19:06:36 +0530

    Fix login page height

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import SubmitButton from "../components/ui/SubmitButton";
 
 export default function Login () {
     return (
-        <div className="flex justify-center sm:items-center sm:h-[100vh]">
+        <div className="flex justify-center sm:items-center sm:h-[90vh]">
             <div className="w-[300px] sm:w-[500px]">
                 <Heading text="Log in"/>
                 <form className="flex flex-col gap-6 items-center">
